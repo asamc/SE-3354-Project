13c263b90434c9c55ca4da7a3a3645ae1c94f639
Asa Mcdaniel <95445304+asamc@users.noreply.github.com>
2022-04-28 13:33:10 -0500

    Update UC1_Implementation.java

--- UC1_Implementation.java
+++ UC1_Implementation.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uc1_implementation;
+//Created by Asa Mcdaniel
+package paintshop;
 
-/**
- *
- * @author SunOfAGambolier
- */
 public class UC1_Implementation {
 
     public static Website ws;
