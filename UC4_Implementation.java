a8fbfb7ed5bb80546337ca5e4148aaa11aebca71
Asa Mcdaniel <95445304+asamc@users.noreply.github.com>
2022-04-28 13:49:35 -0500

    Update UC4_Implementation.java

--- UC4_Implementation.java
+++ UC4_Implementation.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uc1_implementation;
+//Mathew Hoang
+package paintshop;
 
-/**
- *
- * @author SunOfAGambolier
- * @editor Mathew H
- */
 public class UC4_Implementation {
 
     public static Website ws;
