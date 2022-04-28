c00fb474bb09362f1ba235dceb689a76d582eaea
Asa Mcdaniel <95445304+asamc@users.noreply.github.com>
2022-04-28 13:16:57 -0500

    Update Website.java
    fixed package name and added M. Hoang's getProject method

--- Website.java
+++ Website.java
@@ -1,11 +1,17 @@
 //Created by Asa Mcdaniel
-//Edited by William Bell
-package uc1_implementation;
+//Edited by William Bell, Mathew Hoang
+package paintshop;
 
 public class Website {
     Database db;
     public Website(){
         db = new Database();
+    }
+    
+    public int ReqEntInDB(project projIn){
+        int ret = db.addEntry(projIn);
+        db.status--;                                                            //Sends a token to end the waiting status of the Database
+        return ret;                                             //Returns 0 if entry was successfully submitted, 1 if title was already taken
     }
     
     public project deleteEntry(int index, String reason) {			//remove a project from db, returns deleted project
@@ -23,9 +29,7 @@ public class Website {
     		return null;
     }
     
-    public int ReqEntInDB(project projIn){
-        int ret = db.addEntry(projIn);
-        db.status--;                                                            //Sends a token to end the waiting status of the Database
-        return ret;                                             //Returns 0 if entry was successfully submitted, 1 if title was already taken
+    public Project getProject(String title){
+        return db.getProject(title);
     }
 }
